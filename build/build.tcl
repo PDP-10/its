05c496162ec77814ec7abd2c15be6e6545ffa45e
Lars Brinkhoff <lars@nocrew.org>
2016-11-27 22:30:42 +0100

    Build DIR.

--- build/build.tcl
+++ build/build.tcl
@@ -313,6 +313,9 @@ respond "*" ":link channa; rakash pfthmg,dragon; rakash pfthmg\r"
 
 respond "*" ":link sys; ts p,sys; ts peek\r"
 
+respond "*" ":midas sys1;ts dir_bawden;dir^k\r"
+expect ":KILL"
+
 respond "*" ":midas kshack;ts mtboot_kshack;mtboot\r"
 expect ":KILL"
 
