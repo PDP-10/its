f15a175a25839f4ee90cd20889b53ec2ea6c90f3
Eric Swenson <eric@swenson.org>
2016-12-10 16:59:03 -0800

    Added GMSGS and created dummy SYS; :MSGS TIMES file so GMSGS won't get an error.

--- build/build.tcl
+++ build/build.tcl
@@ -843,6 +843,10 @@ respond "*" "debug/0\r"
 type "\033g"
 respond "*" ":link sys;ts pb,sys;ts probe\r"
 
+# GMSGS
+respond "*" ":midas sys2;ts gmsgs_sysen1;gmsgs\r"
+expect ":KILL"
+
 # ndskdmp tape
 respond "*" ":link kshack;good ram,.;ram ram\r"
 respond "*" ":link kshack;ddt bin,.;@ ddt\r"
