202c0f9118663c2bf1a4102ecc74476347049568
Lars Brinkhoff <lars@nocrew.org>
2016-12-17 13:32:19 +0100

    Use Ubuntu 14 in Travis build.
    Its autoconf version is good enough to build KLH10, so we don't have
    to get it from the dns/gnu PPA.

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -1,8 +1,6 @@
 install_linux() {
-    sudo add-apt-repository ppa:dns/gnu -y
     sudo apt-get update -myq
     sudo apt-get install -my expect
-    sudo apt-get install --only-upgrade autoconf
     if test "$EMULATOR" = simh; then
 	sudo apt-get install simh
     fi
