a400a22ec68a05a859a9aa5aee7a447ac6fc4a55
Lars Brinkhoff <lars@nocrew.org>
2018-04-13 12:29:50 +0200

    Move SCHINT to QUUX like RABBIT expects.

--- build/scheme.tcl
+++ build/scheme.tcl
@@ -9,6 +9,19 @@ respond "*" {(load "scheme; nschsy fasl")}
 respond "\n" "(schemedump)"
 respond "==>" "(quit)"
 
+# New Scheme interpreter
+respond "*" "complr\013"
+respond "_" "quux;_schint lsp\r"
+respond "_" "quux;_schmac lsp\r"
+respond "_" "quux;_schuuo lsp\r"
+respond "_" "\032"
+type ":kill\r"
+
+respond "*" ":link quux;scheme interp, schint fasl\r"
+respond "*" ":link quux;scheme macros, schmac fasl\r"
+respond "*" ":link quux;scheme uuohan, schuuo fasl\r"
+respond "*" ":lisp scheme (dump)\r"
+
 # Rabbit Scheme compiler
 respond "*" ":scheme;scheme\r"
 respond "==>" {(schload "scheme; rabbit")}
@@ -24,12 +37,3 @@ respond "==>" {(schload "scheme; rabbit fasl")}
 respond "==>" "(dumpit)"
 respond "Dump anyway" " "
 respond "TS RABBIT" "\r"
-
-# New Scheme interpreter
-respond "*" "complr\013"
-respond "_" "nschem;scheme interp_schint lsp\r"
-respond "_" "nschem;scheme macros_schmac lsp\r"
-respond "_" "nschem;scheme uuohan_schuuo lsp\r"
-respond "_" "\032"
-type ":kill\r"
-respond "*" ":lisp scheme (dump)\r"
