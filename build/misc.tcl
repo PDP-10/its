4ff254411033df258a83eeee9ec2df1545835da5
Lars Brinkhoff <lars@nocrew.org>
2018-04-30 19:15:03 +0200

    Move FAIL and friends to separate script file.

--- build/misc.tcl
+++ build/misc.tcl
@@ -386,25 +386,6 @@ respond "*" ":kill\r"
 respond "*" ":midas sys3;ts ten50_mrc; ten50\r"
 expect ":KILL"
 
-# stktrn
-respond "*" ":cwd sail\r"
-respond "*" ":fail stktrn\r"
-
-# jobdat
-respond "*" "jobdat\r"
-
-# fail
-respond "*" "fail\r"
-respond "*" "\032:kill\r"
-respond "*" ":delete fail bin\r"
-respond "*" ":stink fail\r"
-respond "\n" "\177"
-respond "??" "\033\0331l decsys;decbot bin\r"
-respond "*" ".jbsa/strt\r"
-respond ":" "56/1000\r"
-respond "\n" "\033y sail; fail bin\r"
-respond "*" ":kill\r"
-
 # who%
 respond "*" ":midas sys1;ts who%_sysen3;who%\r"
 expect ":KILL"
