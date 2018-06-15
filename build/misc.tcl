c2c915cad613788932bec30ae1d8f2a96ec39a20
Lars Brinkhoff <lars@nocrew.org>
2018-06-15 10:14:11 +0200

    MACN80 - Z80 cross assembler.

--- build/misc.tcl
+++ build/misc.tcl
@@ -258,6 +258,10 @@ expect ":KILL"
 respond "*" ":midas sys1;ts cross_syseng;cross\r"
 expect ":KILL"
 
+# MACN80
+respond "*" ":midas sys3;ts macn80_gz;macn80\r"
+expect ":KILL"
+
 # dired
 respond "*" ":midas sys;ts dired_sysen2;dired\r"
 expect ":KILL"
