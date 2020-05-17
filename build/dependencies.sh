7db264dc0d45db6c8acf06886904ee87bec36c84
Lars Brinkhoff <lars@nocrew.org>
2020-05-17 19:49:24 +0200

    Add more terminal emulators.
    SIMH KS10 gets VT52.  SIMH KL10 gets VT52 and Tekronix.
    
    KLH10 KS10 doesn't have serial ports, so build no terminal emulators.

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -10,7 +10,8 @@ install_linux() {
     # For GitLab CI
     sudo apt-get install -my git make gcc libncurses-dev autoconf
     case "$EMULATOR" in
-        simh) sudo apt-get install -y simh;;
+        simh) sudo apt-get install -y libegl1-mesa-dev libgles2-mesa-dev
+              sudo apt-get install -y libsdl2-dev;;
         pdp10-k?) sudo apt-get install -y libegl1-mesa-dev libgles2-mesa-dev
               sudo apt-get install -y libx11-dev libxt-dev libsdl2-dev
               sudo apt-get install -y libsdl2-image-dev libpcap-dev
@@ -24,13 +25,16 @@ install_freebsd() {
     pkg install -y gmake git expect
     case "$EMULATOR" in
         pdp10-ka) pkg install -y sdl2 sdl2_image pkgconf gtk3;;
-        pdp10-kl) pkg install -y autoconf;;
+        pdp10-kl) pkg install -y sdl2 pkgconf gtk3 autoconf;;
         klh10) pkg install -y pkgconf autotools;;
     esac
 }
 
 install_osx() {
-    true
+    case "$EMULATOR" in
+        simh) brew install sdl2;;
+        pdp10-*) brew install sdl2;;
+    esac
 }
 
 "$1"
