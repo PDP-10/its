bf7345f12cce810c487c97f886d6de345cee6b43
Lars Brinkhoff <lars@nocrew.org>
2020-09-18 22:54:46 +0200

    DEMST - demon status.

--- build/dm.tcl
+++ build/dm.tcl
@@ -6,3 +6,7 @@ log_progress "ENTERING BUILD SCRIPT: DM"
 # Demon starter.
 respond "*" ":midas sys; atsign demstr_sysen2; demstr\r"
 expect ":KILL"
+
+# Demon status.  Self purifying.
+respond "*" ":midas sysen2; ts demst_sysen2; demst\r"
+expect ":KILL"
