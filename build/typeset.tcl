7520854af5d1d078ef40b2137716fd07a82258a8
Lars Brinkhoff <lars@nocrew.org>
2020-11-30 18:25:15 +0100

    DDT Reference Manual, AI memo 147A.
    Courtesy of Eric Osman.

--- build/typeset.tcl
+++ build/typeset.tcl
@@ -3,3 +3,9 @@ log_progress "ENTERING BUILD SCRIPT: TYPESET"
 respond "*" ":bolio;bolio moon;amber\r"
 respond "NIL" "(quit)"
 expect ":KILL"
+
+# DDT Reference Manual by Eric Osman.
+respond "*" ":otj6\r"
+respond "_" "hur;ddtmem\r"
+respond "_" "\032"
+type ":kill\r"
