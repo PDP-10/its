76e5b7cb8bafb8811f85e0f0cbd88166878aaa3c
Lars Brinkhoff <lars@nocrew.org>
2021-08-26 12:13:26 +0200

    CLU reference manual.
    Written in R.

---
+++ doc/clu/refman.r
@@ -0,0 +1,21 @@
+.so clu/refman.insert
+.so r/sect.rmac
+.de segment
+.  def_seg \0 clu/\0.refman
+.  em
+.sr save_file_name clu/refman.save
+.
+.segment part_a
+.segment lex
+.segment types
+.segment e_d
+.segment action
+.segment exprs
+.segment stmts
+.segment except
+.segment module
+.segment syntax
+.segment opdefs
+.segment io
+.
+.mult_run
