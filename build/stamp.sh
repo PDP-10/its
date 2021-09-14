feb4d770e918a03ed440ecc256436afc3b9ef4d4
Lars Brinkhoff <lars@nocrew.org>
2021-09-14 15:01:15 +0200

    The file timestamps.txt is always in Eastern Standard Time.

--- build/stamp.sh
+++ build/stamp.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # Update timestamps to those listed in the file passed as $1.
+# Those timestamps are always in Eastern Standard Time.
 # If there's no timestamp, we take the latest git author date.
 
 
 list="$PWD/$1"
+OTZ="$TZ"
+export TZ
 
 timestamps(){
     for i in */*; do
@@ -13,8 +16,10 @@ timestamps(){
         if test -n "$t"; then
             set -- $t
             t="$2"
+            TZ=EST
         else
             t=`git log -1 --date=format:%Y%m%d%H%M.%S --format=%cd -- "$1"`
+            TZ="$OTZ"
         fi
         touch -h -t "$t" -- "$i"
     done
