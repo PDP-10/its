da746118aa83aa333727a9e8bdbccdcd86b7d0de
Mike Kostersitz (Oilcan Productions) <77457397+oilcan-productions@users.noreply.github.com>
2023-03-05 10:40:50 -0800

    Update deploy.sh
    Adding FTP deployment to the deploy.sh script

--- build/deploy.sh
+++ build/deploy.sh
@@ -30,4 +30,27 @@ ssh-keyscan -p $PORT -H $HOST >> ~/.ssh/known_hosts
 echo "Deploying to $USER@$HOST:$PORT"
 rsync -av --inplace -e "ssh -p$PORT -l$USER" out/$EMULATOR.tgz $HOST:$DIR
 
+# deploy images to mirror using FTP
+# Define FTP connection details
+
+if test -z "$FTP_SECRET"; then
+    echo 'ERROR: No key in $FTP_SECRET.'
+    exit 1
+fi
+
+FTP_HOST="hactrn.kostersitz.com"
+FTP_USERNAME="hactrn@kostersitz.com"
+FTP_PASSWORD="$FTP_SECRET"
+DIR="images"
+
+echo "Deploying to mirror at hactrn.kostersitz.com"
+
+ftp -n $FTP_HOST <<END_SCRIPT
+quote USER $FTP_USERNAME
+quote PASS $FTP_PASSWORD
+cd $DIR
+put out/$EMULATOR.tgz
+quit
+END_SCRIPT
+
 exit 0
