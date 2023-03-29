199fe1304071b26201f979fafafb7a7d59d4bbb4
Mike Kostersitz (Oilcan Productions) <77457397+oilcan-productions@users.noreply.github.com>
2023-03-28 20:19:49 -0700

    Update zork.tcl
    Fixing a typo

--- build/zork.tcl
+++ build/zork.tcl
@@ -16,7 +16,6 @@ expect -timeout 6000 "Job XXFILE interrupted: .VALUE;"
 type "\033p"
 expect -timeout 500 ":KILL"
 
-respond "*"  ":xxfile lcf;TTY:_lcf;zork xxfile\r"
 expect -timeout 6000 "Job XXFILE interrupted: .VALUE;"
 type "\033p"
 expect -timeout 500 ":KILL"
