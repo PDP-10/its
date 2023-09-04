5204a53d16b9f496be805b69ae1435646b9931a7
Lars Brinkhoff <lars@nocrew.org>
2023-09-04 09:31:44 +0200

    Hello world for C.

---
+++ src/hello/hello.c
@@ -0,0 +1,12 @@
+/* Hello world for the C programming language. */
+
+/* To compile and link, type:
+   :cwd hello
+   :cc hello.c
+   :stinkr hello */
+
+int main()
+{
+	cprint("Hello world\n");
+	return 0;
+}
