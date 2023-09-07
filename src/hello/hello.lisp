15ceb988d846f2105ca58fe08fef048cb62f9900
Lars Brinkhoff <lars@nocrew.org>
2023-09-07 17:31:30 +0200

    Rename Lisp dump script.
    LOADER is for STINK scripts.

--- src/hello/hello.lisp
+++ src/hello/hello.lisp
@@ -1,7 +1,7 @@
 ;;; -*- lisp -*-
 
 ;; Hello world example for Maclisp.  When this is compiled to HELLO
-;; FASL, the file HELLO LOADER will dump out an executable program.
+;; FASL, the file HELLO (DUMP) will dump out an executable program.
 (defun hello ()
   (princ "Hello Maclisp!")
   (terpri)
