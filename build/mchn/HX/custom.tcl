0c555d84b8aa6619a0a9847b3863ad06e50bfa9d
Lars Brinkhoff <lars@nocrew.org>
2023-10-22 13:52:48 +0200

    Gun down TT and HX users that are idle and not logged in.

---
+++ build/mchn/HX/custom.tcl
@@ -0,0 +1,2 @@
+#Gun down users that are idle and not logged in.
+respond "*" ":link dragon; hourly gunner, cstacy; gunner bin\r"
--- build/mchn/TT/custom.tcl
+++ build/mchn/TT/custom.tcl
@@ -2,3 +2,6 @@
 respond "*" ":midas /t device;chaos shutdo_sysnet;shutsr\r"
 respond "end input with ^C" "ALLOW==3036\r\003"
 expect ":KILL"
+
+#Gun down users that are idle and not logged in.
+respond "*" ":link dragon; hourly gunner, cstacy; gunner bin\r"
