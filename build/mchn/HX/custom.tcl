1b182621b1fa7b06d31964296f87e53021a73883
Lars Brinkhoff <lars@nocrew.org>
2023-11-04 15:15:55 +0100

    Weekly backup on TT and HX.

--- build/mchn/HX/custom.tcl
+++ build/mchn/HX/custom.tcl
@@ -1,2 +1,7 @@
 #Gun down users that are idle and not logged in.
 respond "*" ":link dragon; hourly gunner, cstacy; gunner bin\r"
+
+#Run a weekly unattended incremental dump.
+respond "*" ":midas dragon; weekly backup_lars; backup\r"
+respond "HOST NAME =" "177001\r"
+expect ":KILL"
--- build/mchn/TT/custom.tcl
+++ build/mchn/TT/custom.tcl
@@ -5,3 +5,8 @@ expect ":KILL"
 
 #Gun down users that are idle and not logged in.
 respond "*" ":link dragon; hourly gunner, cstacy; gunner bin\r"
+
+#Run a weekly unattended incremental dump.
+respond "*" ":midas dragon; weekly backup_lars; backup\r"
+respond "HOST NAME =" "3036\r"
+expect ":KILL"
